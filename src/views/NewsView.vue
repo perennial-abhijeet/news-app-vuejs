0a881d70e5d2c6583adc890af3d978cc11ca0eaa
Abhijeet Ranmale <abhijeet.ranmale@perennialsys.com>
2023-09-15 17:45:39 +0530

    Added news list to the view

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,13 +1,72 @@
 <template>
   <div>
     <h1 class="page-header">Latest News</h1>
+    <div class="news-list">
+      <NewsCard
+        v-for="article in getNews"
+        :key="article.title"
+        :article="article"
+      />
+    </div>
   </div>
 </template>
 
-<script></script>
+<script>
+import { mapGetters, mapActions } from "vuex";
+import NewsCard from "../components/NewsCard.vue";
+
+export default {
+  components: {
+    NewsCard,
+  },
+  computed: {
+    ...mapGetters(["getNews"]),
+  },
+  methods: {
+    ...mapActions(["fetchNews"]),
+  },
+  mounted() {
+    this.fetchNews();
+  },
+};
+</script>
 
 <style scoped>
 .page-header {
   text-align: center;
+}
+
+.news-list {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+}
+
+@media (max-width: 865px) {
+  .news-card {
+    width: 100%;
+    margin: 10;
+  }
+}
+
+@media (min-width: 866px) {
+  .news-card {
+    width: calc(46% - 20px);
+    margin: 10px;
+  }
+}
+
+@media (min-width: 1400px) {
+  .news-card {
+    width: calc(30% - 20px);
+    margin: 10px;
+  }
+}
+
+@media (min-width: 1716px) {
+  .news-card {
+    width: calc(23% - 20px);
+    margin: 10px;
+  }
 }
 </style>
